46b5f9625962bea55956a52fadd1b8d3dffc3be7
jesse <m17521137221@163.com>
2018-10-18 20:20:06 +0800

    添加了item-service开启mapper注解扫描

--- leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemServiceApplication.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/LeyouItemServiceApplication.java
@@ -1,5 +1,6 @@
 package com.leyou;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.item.mapper")
 public class LeyouItemServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(LeyouItemServiceApplication.class,args);
